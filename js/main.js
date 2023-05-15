44582490ea9e7ff1a06c26df65036590592caba1
Marco Giammari <marcogiammari@gmail.com>
2023-05-15 10:21:35 +0200

    refactor: add comments

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // array generato in mounted
             emojis: [
                 
             ],
+            // variabile contatore per la chat attiva
             currentChat: 0,
+            // variabili di appoggio per l'input di ricerca contatti e di nuovo messaggio
             inputSearch: "",
             inputMsg: {
                 date: "",
@@ -182,44 +185,60 @@ createApp({
         };
     },
     methods: {
+        // rende visibili o nasconde i div dei contatti nel contenitore sinistro
         getDisplayValue(i) {
             return this.contacts[i].visible ? "d-flex" : "d-none"
         },
         writeNewMsg() {
+            // oggetto messaggio inviato dall'utente
             let newMsg = {
+                // prende data e orario attuali e li formatta in due cifre
                 date: DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss').toLocaleString({ month: '2-digit', hour: '2-digit', minute: '2-digit'}),
                 message: this.inputMsg.message,
                 status: "sent",
             };
+            // oggetto messaggio di risposta automatica con emoji random
             let newAnswer = {
                 date: DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss').toLocaleString({ month: '2-digit', hour: '2-digit', minute: '2-digit'}),
                 message: this.emojis[Math.floor(Math.random() * this.emojis.length)],
                 status: "received"
             }
+            // aggiunge il messaggio utente all'array bindato e diventa visibile in pagina 
             this.contacts[this.currentChat].messages.push(newMsg);
             x = document.getElementById("chat-wrapper")
+            // scrolla la scrollbar fino all'altezza del contenitore per far visualizzare il messaggio
             setTimeout(() => x.scrollTo(0, x.scrollHeight), 10)
+            // aggiunge il messaggio di risposta automatica
             setTimeout(() => this.contacts[this.currentChat].messages.push(newAnswer), 1000)
+            // scrolla di nuovo
             setTimeout(() => x.scrollTo(0, x.scrollHeight), 1010)
+            // riazzera l'input
             this.inputMsg.message = ""
         },
         chooseEmoji(i) {
+            // al click l'emoji viene portata nell'input di nuovo messaggio, che viene focalizzato
             this.inputMsg.message += this.emojis[i];
             document.getElementById("input-bar").focus();
         },
         getLastMsg(i) {
+            // ritorna l'ultimo messaggio della chat (controllando che ce ne sia almeno uno)
             if (this.contacts[i].messages.length > 0) {
               return this.contacts[i].messages.at(-1).message  
             } 
         },
         getTimeDiff(date) {
+            // ritorna il tempo passato dal messaggio tramite la libreria Luxon
             if (date) {
+                // formattazione delle date originarie 
                 const d = date.split(" ")
                 d[0] = d[0].split("/").reverse().join("-")
+                // creazione oggetto data con luxon e formattazione a due cifre
                 let luxonDate = DateTime.fromISO(d[0] + "T" + d[1] + "+02:00");
                 let time = luxonDate.toLocaleString({ hour: '2-digit', minute: '2-digit'}); 
+                // calcolo della differenza temporale tra il messaggio e il momento presente
                 const diff = DateTime.now().diff((luxonDate), 'days').days
                 let howLongAgo = DateTime.now().minus({ days: diff }).toRelativeCalendar()
+                // se il messaggio è di oggi lasciamo che sia visualizzato l'orario
                 if (howLongAgo != "oggi") {
                     return howLongAgo
                 }
@@ -227,22 +246,28 @@ createApp({
             }
         },
         deleteMsg(i) {
+            // cancella il messaggio con il metodo slpice e l'indice in argomento
             this.contacts[this.currentChat].messages.splice(i, 1);
-            console.log(this.contacts[this.currentChat].messages)
+            // se non ci sono più messaggi viene cancellata anche la chat del contenitore sinistro
             if (this.contacts[this.currentChat].messages.length == 0) {
                 this.contacts[this.currentChat].visible = false;
             }
         },
         searchContacts() {
+            // cerca corrispondenze tra l'input della searchbar e i nomi presenti in contacts
             this.contacts.forEach(e => {
+                // lowercase per intercettare maiuscole e minuscole
                 let toCheck = e.name.toLowerCase()
+                // usa lo slice per confrontare l'input con lo stesso numero di lettere iniziali dei nomi
                 toCheck = toCheck.slice(0,this.inputSearch.length)
                 toCheck == this.inputSearch.toLowerCase() ? e.visible = true : e.visible = false;
             })
         }
     },
     mounted() {
+        // generatore di emoji 
         const emojiRange = [128513, 128579];
+        // ciclo for che scorre i numeri del range per creare le emoji
         for (let i = emojiRange[0]; i < emojiRange[1]; i++) {
             const emoticon = String.fromCodePoint(i)
             this.emojis.push(emoticon);
